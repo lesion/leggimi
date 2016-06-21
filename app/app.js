cc532467ebf422b69cd8c4167c977c855c8c8fba
lesion <lesion@autistici.org>
2016-06-21 16:06:46 +0200

    importo i vendor da qui

--- app/app.js
+++ app/app.js
@@ -1,26 +1,30 @@
-// Node.js modules and those from npm
-// are required the same way as always.
-// const app = require('remote').require('app')
-// const jetpack = require('fs-jetpack').cwd(app.getAppPath())
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import VueMdl from 'vue-mdl'
+import Keen from 'keen-ui'
+
+import 'normalize.css/normalize.css'
+import 'keen-ui/dist/min/keen-ui.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.scss'
+
+// TOFIX: qui devo importare solo la famiglia che interessa non tutto il polpettone !
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import './stylesheets/main.sass'
+
+Vue.use(VueRouter)
+Vue.use(Keen)
+
 import games from './components/Games.vue'
 import welcome from './components/Welcome.vue'
 import reader from './components/Reader.vue'
 import writer from './components/Writer.vue'
-Vue.use(VueRouter)
-Vue.use(VueMdl)
-
-require('material-design-lite-bundled')
 
 var router = new VueRouter()
 router.map({
   '/games': {
     component: games
   },
-  '/reader': {
+  '/reader/:story_id': {
     component: reader
   },
   '/writer': {
@@ -31,8 +35,9 @@ router.map({
   }
 })
 
+router.alias({ '/reader': '/reader/-1' })
+
 var App = Vue.extend({})
-console.log('starting')
 
 router.start(App, '#app')
-router.go('/welcome')
+router.go('/reader')
